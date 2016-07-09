fcc1c5290562c22bfe5cc4b47dedf52723fa5da3
Noah Apthorpe <apthorpe@princeton.edu>
2016-07-09 15:06:43 -0400

    updated load data and preprocessing with parameters and correct filenames

--- preprocessing.py
+++ preprocessing.py
@@ -7,8 +7,8 @@
 # Description: Video contrast improvement
 #   and ROI centroid conversion
 # 
-# Usage: pass data from load_data in load.py
-#   consecutively to the functions in this
+# Usage: pass data and file_names  from load_data 
+#   in load.py  consecutively to the functions in this
 #   file to produce .tif files ready for ZNN 
 #
 ###################################################
@@ -17,36 +17,36 @@ import numpy as np
 from PIL import Image
 import os.path
 
-def improve_contrast(data):
+def improve_contrast(data, upper_contrast, lower_contrast):
     new_data = []
     for i, (stk,roi) in enumerate(data):
-        low_p = np.percentile(stk.flatten(), 3)
-        high_p = np.percentile(stk.flatten(), 99)
+        low_p = np.percentile(stk.flatten(), lower_contrast)
+        high_p = np.percentile(stk.flatten(), upper_constrast)
         new_stk = np.clip(stk, low_p, high_p)
         new_stk = new_stk - new_stk.mean()
         new_stk = np.divide(new_stk-np.min(new_stk), np.max(new_stk) - np.min(new_stk))
         new_data.append((new_stk, roi))
     return new_data
 
-def get_centroids(data, radius): 
+def get_centroids(data, radius, img_width, img_height): 
     new_data = []
     for j,(stk, rois) in enumerate(data):
-        new_rois = np.zeros(rois.shape)                                                             
+        new_rois = np.zeros(rois.shape)
         for i,r in enumerate(rois):  
             cx,cy = np.where(r!=0)                                                  
             cx,cy = int(cx.mean()), int(cy.mean())  
-            x,y = np.ogrid[0:512, 0:512]
+            x,y = np.ogrid[0:img_width, 0:img_height]
             index = (x-cx)**2 + (y-cy)**2 <= radius**2
             new_rois[i, index] = 1 
         new_data.append((stk,new_rois))
     return new_data
 
-def save_tifs(data, directory):
+def save_tifs(data, file_names, directory):
     if directory[-1] != os.path.sep:
         directory += os.path.sep
     for i,(stk,roi) in enumerate(data):
-        stk_name = directory + "stk{}".format(i+1) + ".tif"
-        roi_name = directory + "roi{}".format(i+1) + ".tif"
+        stk_name = directory + file_names[i] + ".tif"
+        roi_name = directory + file_names[i] + ".tif"
         im_stk = Image.fromarray(stk.squeeze())
         im_roi = Image.fromarray(roi)
         im_stk.save(stk_name)
