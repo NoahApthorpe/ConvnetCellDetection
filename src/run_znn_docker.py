b39cd90a392372b7d5b2eeff8f4fb622869ba175
AlexanderRiordan <riordan.alexander@gmail.com>
2016-08-31 23:48:07 -0400

    added auto-shutdown of Docker container and machine

--- src/run_znn_docker.py
+++ src/run_znn_docker.py
@@ -26,10 +26,10 @@ def start_docker_machine(memory):
     return cmd
     
     
-def start_znn_container(dir_to_mount):
+def start_znn_container(dir_to_mount, container_name):
     cmd = ''
-    cmd += 'docker run -v ' + dir_to_mount + ':/opt/znn-release/ConvnetCellDetection -it jpwu/znn:v0.1.4 '
-    cmd += '/bin/bash -c ' 
+    cmd += 'docker run -v ' + dir_to_mount + ':/opt/znn-release/ConvnetCellDetection '  
+    cmd += '--name ' + container_name + ' -it jpwu/znn:v0.1.4 ' + '/bin/bash -c ' 
     return cmd
 
 
@@ -42,6 +42,12 @@ def train_network(output_dir):
 def forward_pass(output_dir):
     cmd = ''
     cmd += '"cd opt/znn-release/python; sudo ldconfig; python forward.py -c ' + output_dir + '/znn_config.cfg"'
+    return cmd
+
+def remove_znn_container(container_name):
+    cmd = ''
+    cmd += '; docker stop ' + container_name + '; docker rm ' + container_name + '; docker-machine stop'
+    #TODO: change default docker machine to user-given name
     return cmd
 
 '''
@@ -64,21 +70,25 @@ if __name__ == "__main__":
     main_config_fpath = '../main_config_ar.cfg'
     cfg_parser.readfp(open('../main_config_ar.cfg', 'r'))
     memory = cfg_parser.get('docker', 'memory')
+    container_name = cfg_parser.get('docker', 'container_name')
     training_output_dir = cfg_parser.get('training', 'training_output_dir')
     forward_output_dir = cfg_parser.get('forward', 'forward_output_dir')
     
     run_type = 'forward' #this will be set in main_config or pipeline.py
+    dir_to_mount = '/Users/sergiomartinez/Documents/ConvnetCellDetection' #""
+    
     
     cmd = ''
     cmd += start_docker_machine(memory)
     cmd += '; '
-    cmd += start_znn_container('/Users/sergiomartinez/Documents/ConvnetCellDetection')
+    cmd += start_znn_container(dir_to_mount, container_name)
     
     if run_type == 'training':
-        cmd += train_network(dockerize_path(training_output_dir))
+        cmd += train_network(dockerize_path(training_output_dir)) + remove_znn_container(container_name)
     elif run_type == 'forward':
-        cmd += forward_pass(dockerize_path(forward_output_dir))
+        cmd += forward_pass(dockerize_path(forward_output_dir)) + remove_znn_container(container_name)
     else:
+        cmd += remove_znn_container(container_name)
         raise ValueError('run_type variable should be one of "forward" or "training"', run_type)
     
     process = subprocess.Popen(cmd, shell=True)
